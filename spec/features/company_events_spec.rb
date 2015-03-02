b9ce81aee98cb4095e3f4415424fbe7ef4e10918
Annie Lydens <aelydens@gmail.com>
2015-03-01 18:55:13 -0700

    Add bcrypt for user auth

--- spec/features/company_events_spec.rb
+++ spec/features/company_events_spec.rb
@@ -8,7 +8,6 @@ feature 'Company Events' do
     event_2 = Event.create(name: "Little Party", capacity: 6, company_id: company.id)
 
     visit company_events_path(company)
-    save_and_open_page
     expect(page).to have_content("Big Party")
     expect(page).to have_content("Little Party")
     expect(page).to have_content("Happy Corp")
